71de9a538e5f60d949c6f79c438535159d885224
SupunTJ <jayaweeraspn@gmail.com>
2024-06-28 12:09:10 +0530

    new update

--- pipeline.groovy
+++ pipeline.groovy
@@ -12,23 +12,47 @@ pipeline {
         stage('Build Docker Image') {
             steps {
                 script {
-                    sh 'docker build -t Supun3998/frontend-app-image .'
+                    sh 'docker build -t supun3998/frontend-app-image .'
                 }
             }
         }
+
+      
         stage('Run Docker Container') {
             steps {
                 script {
                     // Stop any running containers with the same name
-                    
+                    sh 'docker stop server-frontend || true'
+                    sh 'docker rm server-frontend || true'
                     // Run the new container
-                    sh 'docker run -d -p 3001:3000 Supun3998/frontend-app-image'
+                    sh 'docker run -d -p 3001:3000 --name frontend-container supun3998/frontend-app-image'
                 }
             }
         }
         stage('Show Running Containers') {
             steps {
                 sh 'docker ps'
+            }
+        }
+
+        stage('Login to Docker Hub') {
+            steps {
+                withCredentials([string(credentialsId: 'dockerhubpwd', variable: 'Dockerhub')]) {
+                    script {
+                        sh "docker login -u supun3998 -p ${Dockerhub}"
+                    }
+                }
+            }
+        }
+
+        stage('Push Image') {
+            steps {
+                script {
+                    retry(3) {
+                        echo 'Pushing frontend image to Docker Hub...'
+                        sh 'docker push supun3998/frontend-app-image'
+                    }
+                }
             }
         }
     }
